0d85413f78f655b7b11ab716b3bdf35b1c07f183
Alteri <trochonowicz.jacek@gmail.com>
2022-05-21 08:46:03 +0200

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import City from "./City";
 
 const App = () => {
-  return <div>test</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/:city" element={<City />} />
+    </Routes>
+  );
 };
 
 export default App;

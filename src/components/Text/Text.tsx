3b730933db4f87ba531699228d2dde0c1a51f8b1
Alteri <trochonowicz.jacek@gmail.com>
2022-05-22 17:33:30 +0200

    add text component

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode } from "react";
+import { TextStyled } from "./styled";
+
+const textTypes = ["h1", "h2", "p"] as const;
+export type TextType = typeof textTypes[number];
+
+const textTypeToTag = {
+  h1: "h1",
+  h2: "h2",
+  p: "p",
+} as const;
+
+export type TextProps = {
+  children: ReactNode;
+  textType?: TextType;
+};
+
+const Text = ({ children, textType = "p", ...props }: TextProps) => {
+  return (
+    <TextStyled as={textTypeToTag[textType]} textType={textType} {...props}>
+      {children}
+    </TextStyled>
+  );
+};
+
+export default Text;

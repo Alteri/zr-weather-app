645b49adc29561948abcd65cd5668ee36a6186e2
Alteri <trochonowicz.jacek@gmail.com>
2022-05-23 19:32:39 +0200

    localStorage

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { PageStyled } from './styled';
-import { addCompareCities } from '../../store/fetch';
+import { addCompareCities, fetchCity } from '../../store/fetch';
 
 type PageProps = {
   children: ReactNode;
@@ -9,9 +9,12 @@ type PageProps = {
 
 const Page = ({ children }: PageProps) => {
   const dispatch = useDispatch();
+  const currentCity = localStorage.getItem('currentCity') || '';
+  const initialCity = currentCity && JSON.parse(currentCity);
 
   useEffect(() => {
     dispatch(addCompareCities());
+    initialCity && dispatch(fetchCity(initialCity));
   }, [dispatch]);
 
   return <PageStyled>{children}</PageStyled>;

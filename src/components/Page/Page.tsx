d2b2ce70d92ef462f15ad8b93da973c3b07fb9af
Alteri <trochonowicz.jacek@gmail.com>
2022-05-24 08:24:22 +0200

    localStorage

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,7 +1,9 @@
 import React, { ReactNode, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { PageStyled } from './styled';
 import { addCompareCities, fetchCity } from '../../store/fetch';
+import { slugify } from '../../utils/slugify';
 
 type PageProps = {
   children: ReactNode;
@@ -9,12 +11,16 @@ type PageProps = {
 
 const Page = ({ children }: PageProps) => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const currentCity = localStorage.getItem('currentCity') || '';
   const initialCity = currentCity && JSON.parse(currentCity);
 
   useEffect(() => {
     dispatch(addCompareCities());
-    initialCity && dispatch(fetchCity(initialCity));
+    if (initialCity) {
+      dispatch(fetchCity(initialCity));
+      navigate(`/${slugify(initialCity)}`, { replace: true });
+    }
   }, [dispatch]);
 
   return <PageStyled>{children}</PageStyled>;
